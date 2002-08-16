945fbfe9207c6110f43539dac48074dd429ae213
Alberto Griggio <agriggio@users.noreply.github.com>
2002-08-16 10:05:35 +0000

    experimental C++ code generator

--- widgets/button/codegen.py
+++ widgets/button/codegen.py
@@ -26,7 +26,6 @@ def python_code_generator(obj):
     props_buf = pygen.generate_common_properties(obj)
     return init, props_buf, []
 
-
 def xrc_code_generator(obj):
     xrcgen = common.code_writers['XRC']
     class ButtonXrcObject(xrcgen.DefaultXrcObject):
@@ -47,6 +46,28 @@ def xrc_code_generator(obj):
     return ButtonXrcObject(obj)
 
 
+def cpp_code_generator(obj):
+    """\
+    fuction that generates python code for wxButton objects.
+    """
+    cppgen = common.code_writers['C++']
+    prop = obj.properties
+    id_name, id = cppgen.generate_code_id(obj)
+    if id_name: ids = [ '%s = %s' % (id_name, id) ]
+    else: ids = []
+    if not obj.parent.is_toplevel: parent = '%s' % obj.parent.name
+    else: parent = 'this'
+    label = '"' + prop.get('label', '').replace('"', r'\"') + '"'
+    if obj.is_toplevel:
+        l = ['%s = new %s(%s, %s, %s);\n' % (obj.name, obj.klass, parent,
+                                             id, label)]
+        return l , ids, [], []    
+    init = [ '%s = new wxButton(%s, %s, %s);\n' % 
+             (obj.name, parent, id, label) ]
+    props_buf = cppgen.generate_common_properties(obj)
+    return init, ids, props_buf, []
+
+
 def initialize():
     common.class_names['EditButton'] = 'wxButton'
     pygen = common.code_writers.get('python')
@@ -55,3 +76,11 @@ def initialize():
     xrcgen = common.code_writers.get("XRC")
     if xrcgen:
         xrcgen.add_widget_handler('wxButton', xrc_code_generator)
+    cppgen = common.code_writers.get('C++')
+    if cppgen:
+        constructor = [('wxWindow*', 'parent'), ('int', 'id'),
+                       ('const wxString&', 'label'),
+                       ('const wxPoint&', 'pos', 'wxDefaultPosition'),
+                       ('const wxSize&', 'size', 'wxDefaultSize'),
+                       ('long', 'style', '0')]
+        cppgen.add_widget_handler('wxButton', cpp_code_generator, constructor)
