d2ca83f8b9ed96bab50719863e2e7f57ba151c42
Alberto Griggio <agriggio@users.sourceforge.net>
2003-03-11 23:26:59 +0000

    wxTreeCtrl support

---
+++ widgets/tree_ctrl/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py: tree ctrl widget module initialization
+#
+# Copyright (c) 2002-2003 Alberto Griggio <albgrig@tiscalinet.it>
+# License: MIT (see license.txt)
+# THIS PROGRAM COMES WITH NO WARRANTY
+
+def initialize():
+    import common
+    import codegen
+    codegen.initialize()
+    if common.use_gui:
+        import tree_ctrl
+        return tree_ctrl.initialize()
