5d3e89ee9134009b41b3f7488594a132efe4aa86
crazyinsomniac <crazyinsomniac@localhost>
2003-06-27 05:56:55 +0000

    added ScrolledWindow support

--- widgets/panel/perl_codegen.py
+++ widgets/panel/perl_codegen.py
@@ -1,5 +1,5 @@
 # perl_codegen.py : perl generator functions for wxPanel objects
-# $Id: perl_codegen.py,v 1.2 2003/06/25 23:51:26 crazyinsomniac Exp $
+# $Id: perl_codegen.py,v 1.3 2003/06/27 05:56:55 crazyinsomniac Exp $
 #
 # Copyright (c) 2002-2003 D.H. aka crazyinsomniac on sourceforge.net
 # License: MIT (see license.txt)
@@ -9,27 +9,60 @@ import common
 
 class PerlCodeGenerator:
     def get_code(self, panel):
-        pygen = common.code_writers['perl']
+        plgen = common.code_writers['perl']
         prop = panel.properties
-        id_name, id = pygen.generate_code_id(panel)
+        try:
+            scrollable = int(prop['scrollable'])
+        except:
+            scrollable = False
+
+        id_name, id = plgen.generate_code_id(panel)
         if not panel.parent.is_toplevel:
             parent = '$self->{%s}' % panel.parent.name
-        else: parent = '$self'
+        else:
+            parent = '$self'
+
         if panel.is_toplevel:
             l = []
             if id_name: l.append(id_name)
             l.append('$self->{%s} = %s->new(%s, %s);\n' %
                  (panel.name, panel.klass.replace('wx','Wx::',1), parent, id))
             return l, [], []
+
         init = []
         if id_name: init.append(id_name)
         style = prop.get("style", 'wxTAB_TRAVERSAL')
-        if style != 'wxTAB_TRAVERSAL': style = "%s" % style
-        else: style = ''
-        init.append('$self->{%s} = Wx::Panel->new(%s, %s, \
-wxDefaultPosition, wxDefaultSize, %s);\n' % (panel.name, parent, id, style))
-        props_buf = pygen.generate_common_properties(panel)
+        if not( scrollable or style != 'wxTAB_TRAVERSAL' ):
+            style = ''
+        if scrollable:
+            klass = 'Wx::ScrolledWindow'
+        else:
+            klass = 'Wx::Panel'
+
+        init.append('$self->{%s} = %s->new(%s, %s, \
+wxDefaultPosition, wxDefaultSize, %s);\n'
+            % (panel.name, klass, parent, id, style))
+
+        props_buf = plgen.generate_common_properties(panel)
+        if scrollable:
+            sr = prop.get('scroll_rate', '0, 0')
+            props_buf.append('$self->{%s}->SetScrollRate(%s);\n'
+                % (panel.name, sr))
         return init, props_buf, []
+
+    def get_properties_code(self, obj):
+        plgen = common.code_writers['perl']
+        prop = obj.properties
+        try:
+            scrollable = int(prop['scrollable'])
+        except:
+            scrollable = False
+
+        props_buf = plgen.generate_common_properties(obj)
+        if scrollable:
+            sr = prop.get('scroll_rate', '0, 0')
+            props_buf.append('$self->SetScrollRate(%s);\n' % sr)
+        return props_buf
 
 # end of class PerlCodeGenerator
 
@@ -43,3 +76,4 @@ def initialize():
     plgen = common.code_writers.get('perl')
     if plgen:
         plgen.add_widget_handler('wxPanel', PerlCodeGenerator())
+        plgen.add_widget_handler('wxScrolledWindow', PerlCodeGenerator())
