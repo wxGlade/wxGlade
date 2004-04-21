2a7f65783fdd2183cd324e30dc76682e8227aa27
Alberto Griggio <agriggio@users.noreply.github.com>
2004-04-21 23:41:48 +0000

    to turn wxGlade into a package

---
+++ __init__.py
@@ -0,0 +1,6 @@
+# __init__.py: to turn wxGlade into a package
+# $Id: __init__.py,v 1.1 2004/04/21 23:41:48 agriggio Exp $
+#
+# Copyright (c) 2002-2004 Alberto Griggio <albgrig@tiscalinet.it>
+# License: MIT (see license.txt)
+# THIS PROGRAM COMES WITH NO WARRANTY
