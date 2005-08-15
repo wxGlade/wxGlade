5a99315e03ca9e540c56d8298de2e52a03c1d54f
crazyinsomniac <crazyinsomniac@localhost>
2005-08-15 07:53:15 +0000

    discovered by 1255968 (wxFoo Wx::Foo)

--- widgets/toggle_button/perl_codegen.py
+++ widgets/toggle_button/perl_codegen.py
@@ -1,5 +1,5 @@
 # perl_codegen.py : perl generator functions for wxToggleButton objects
-# $Id: perl_codegen.py,v 1.3 2004/09/17 13:09:49 agriggio Exp $
+# $Id: perl_codegen.py,v 1.4 2005/08/15 07:53:15 crazyinsomniac Exp $
 #
 # Copyright (c) 2002-2004 D.H. aka crazyinsomniac on sourceforge.net
 # License: MIT (see license.txt)
@@ -21,8 +21,12 @@ class PerlCodeGenerator:
 
         if id_name: init.append(id_name)
 
+        klass = obj.base;
+        if klass != obj.klass : klass = obj.klass; 
+        else: klass = klass.replace('wx','Wx::',1);
+
         init.append('$self->{%s} = %s->new(%s, %s, %s);\n' %
-            (obj.name, obj.klass.replace('wx','Wx::',1), parent, id, label))
+            (obj.name, klass, parent, id, label))
         props_buf = plgen.generate_common_properties(obj)
 
         value = prop.get('value')
