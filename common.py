f1806a518122a0a2a04da93ba8bf632fc2f00bf9
Alberto Griggio <agriggio@users.sourceforge.net>
2006-03-02 09:02:43 +0000

    updated version number

--- common.py
+++ common.py
@@ -1,5 +1,5 @@
 # common.py: global variables
-# $Id: common.py,v 1.50 2005/10/08 14:00:26 agriggio Exp $
+# $Id: common.py,v 1.51 2006/03/02 09:02:43 agriggio Exp $
 # 
 # Copyright (c) 2002-2005 Alberto Griggio <agriggio@users.sourceforge.net>
 # License: MIT (see license.txt)
@@ -12,7 +12,7 @@ import os
 use_gui = True
 
 # version identification string
-version = '0.4'
+version = '0.4.1'
 
 # program path, set in wxglade.py
 wxglade_path = '.'
