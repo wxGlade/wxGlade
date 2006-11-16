16a34df509190dfc7b31a4ea97d3e658746a2780
guyru <guyru@localhost>
2006-11-16 15:20:54 +0000

    moved to the wxPython namespace

--- widgets/tree_ctrl/tree_ctrl.py
+++ widgets/tree_ctrl/tree_ctrl.py
@@ -1,11 +1,11 @@
 # text_ctrl.py: wxTreeCtrl objects
-# $Id: tree_ctrl.py,v 1.9 2005/07/11 12:12:42 agriggio Exp $
+# $Id: tree_ctrl.py,v 1.10 2006/11/16 15:20:54 guyru Exp $
 #
 # Copyright (c) 2002-2005 Alberto Griggio <agriggio@users.sourceforge.net>
 # License: MIT (see license.txt)
 # THIS PROGRAM COMES WITH NO WARRANTY
 
-from wxPython.wx import *
+import wx
 from edit_windows import ManagedBase
 from tree import Tree
 import common, misc
@@ -14,7 +14,7 @@ from widget_properties import *
 
 class EditTreeCtrl(ManagedBase):
     """\
-    Class to handle wxTreeCtrl objects
+    Class to handle wx.TreeCtrl objects
     """
 
     events = [
@@ -39,23 +39,23 @@ class EditTreeCtrl(ManagedBase):
         ]
     
     def __init__(self, name, parent, id, sizer, pos, property_window,
-                 show=True, style=wxTR_HAS_BUTTONS|wxSUNKEN_BORDER):
+                 show=True, style=wx.TR_HAS_BUTTONS|wx.SUNKEN_BORDER):
         ManagedBase.__init__(self, name, 'wxTreeCtrl', parent, id, sizer, pos,
                              property_window, show=show)
         self.style = style
         self.access_functions['style'] = (self.get_style, self.set_style)
         # style property
-        self.style_pos  = (wxTR_HAS_BUTTONS, wxTR_NO_LINES, wxTR_LINES_AT_ROOT,
-                           wxTR_EDIT_LABELS, wxTR_MULTIPLE, wxTR_NO_BUTTONS,
-                           wxTR_TWIST_BUTTONS, wxTR_FULL_ROW_HIGHLIGHT,
-                           wxTR_HIDE_ROOT, wxTR_ROW_LINES,
-                           wxTR_HAS_VARIABLE_ROW_HEIGHT,
-                           wxTR_SINGLE, wxTR_MULTIPLE, wxTR_EXTENDED,
-                           wxTR_DEFAULT_STYLE, wxSIMPLE_BORDER, wxDOUBLE_BORDER,
-                           wxSUNKEN_BORDER, wxRAISED_BORDER, wxSTATIC_BORDER,
-                           wxNO_BORDER, wxWANTS_CHARS, 
-                           wxNO_FULL_REPAINT_ON_RESIZE,
-                           wxFULL_REPAINT_ON_RESIZE)
+        self.style_pos  = (wx.TR_HAS_BUTTONS, wx.TR_NO_LINES, wx.TR_LINES_AT_ROOT,
+                           wx.TR_EDIT_LABELS, wx.TR_MULTIPLE, wx.TR_NO_BUTTONS,
+                           wx.TR_TWIST_BUTTONS, wx.TR_FULL_ROW_HIGHLIGHT,
+                           wx.TR_HIDE_ROOT, wx.TR_ROW_LINES,
+                           wx.TR_HAS_VARIABLE_ROW_HEIGHT,
+                           wx.TR_SINGLE, wx.TR_MULTIPLE, wx.TR_EXTENDED,
+                           wx.TR_DEFAULT_STYLE, wx.SIMPLE_BORDER, wx.DOUBLE_BORDER,
+                           wx.SUNKEN_BORDER, wx.RAISED_BORDER, wx.STATIC_BORDER,
+                           wx.NO_BORDER, wx.WANTS_CHARS, 
+                           wx.NO_FULL_REPAINT_ON_RESIZE,
+                           wx.FULL_REPAINT_ON_RESIZE)
         style_labels = ('#section#Style', 'wxTR_HAS_BUTTONS', 'wxTR_NO_LINES',
                         'wxTR_LINES_AT_ROOT', 'wxTR_EDIT_LABELS',
                         'wxTR_MULTIPLE', 'wxTR_NO_BUTTONS',
@@ -73,8 +73,8 @@ class EditTreeCtrl(ManagedBase):
         self._item_with_name = None
 
     def create_widget(self):
-        self.widget = wxTreeCtrl(self.parent.widget, self.id,
-                                 style=wxTR_HAS_BUTTONS|wxSUNKEN_BORDER)
+        self.widget = wx.TreeCtrl(self.parent.widget, self.id,
+                                 style=wx.TR_HAS_BUTTONS|wx.SUNKEN_BORDER)
         # add a couple of items just for a better appearence
         root = self.widget.AddRoot(' Tree Control:')
         self._item_with_name = self.widget.AppendItem(root, ' ' + self.name)
@@ -93,11 +93,11 @@ class EditTreeCtrl(ManagedBase):
             
     def create_properties(self):
         ManagedBase.create_properties(self)
-        panel = wxScrolledWindow(self.notebook, -1, style=wxTAB_TRAVERSAL) 
+        panel = wx.ScrolledWindow(self.notebook, -1, style=wx.TAB_TRAVERSAL) 
         prop = self.properties
         prop['style'].display(panel)
-        szr = wxBoxSizer(wxVERTICAL)
-        szr.Add(prop['style'].panel, 0, wxEXPAND)
+        szr = wx.BoxSizer(wx.VERTICAL)
+        szr.Add(prop['style'].panel, 0, wx.EXPAND)
         panel.SetAutoLayout(True)
         panel.SetSizer(szr)
         szr.Fit(panel)
@@ -134,7 +134,7 @@ def builder(parent, sizer, pos, number=[1]):
     while common.app_tree.has_name(name):
         number[0] += 1
         name = 'tree_ctrl_%d' % number[0]
-    tree_ctrl = EditTreeCtrl(name, parent, wxNewId(), sizer, pos,
+    tree_ctrl = EditTreeCtrl(name, parent, wx.NewId(), sizer, pos,
                              common.property_panel)
     node = Tree.Node(tree_ctrl)
     tree_ctrl.node = node
@@ -142,7 +142,7 @@ def builder(parent, sizer, pos, number=[1]):
     tree_ctrl.set_flag("wxEXPAND")
     tree_ctrl.show_widget(True)
     common.app_tree.insert(node, sizer.node, pos-1)
-    sizer.set_item(tree_ctrl.pos, 1, wxEXPAND)
+    sizer.set_item(tree_ctrl.pos, 1, wx.EXPAND)
 
 
 def xml_builder(attrs, parent, sizer, sizeritem, pos=None):
@@ -154,7 +154,7 @@ def xml_builder(attrs, parent, sizer, sizeritem, pos=None):
     except KeyError: raise XmlParsingError, "'name' attribute missing"
     if sizer is None or sizeritem is None:
         raise XmlParsingError, "sizer or sizeritem object cannot be None"
-    tree_ctrl = EditTreeCtrl(name, parent, wxNewId(), sizer, pos,
+    tree_ctrl = EditTreeCtrl(name, parent, wx.NewId(), sizer, pos,
                              common.property_panel, style=0)
     sizer.set_item(tree_ctrl.pos, option=sizeritem.option, flag=sizeritem.flag,
                    border=sizeritem.border)
@@ -167,7 +167,7 @@ def xml_builder(attrs, parent, sizer, sizeritem, pos=None):
 
 def initialize():
     """\
-    initialization function for the module: returns a wxBitmapButton to be
+    initialization function for the module: returns a wx.BitmapButton to be
     added to the main palette.
     """
     common.widgets['EditTreeCtrl'] = builder
