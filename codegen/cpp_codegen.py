089807837450e6cdf86076a7224ca1b00c2aad90
Alberto Griggio <griggio@dit.unitn.it>
2008-10-21 14:34:29 +0200

    fixed duplication of 'extra code' in C++ output

--- codegen/cpp_codegen.py
+++ codegen/cpp_codegen.py
@@ -1200,12 +1200,12 @@ def add_class(code_obj):
         swrite = output_source.write
         for line in source_buffer: swrite(line)
         # extra code
-        if classes[code_obj.klass].extra_code_h:
-            _current_extra_code_h.extend(
-                classes[code_obj.klass].extra_code_h[::-1])
-        if classes[code_obj.klass].extra_code_cpp:
-            _current_extra_code_cpp.extend(
-                classes[code_obj.klass].extra_code_h[::-1])
+        ## if classes[code_obj.klass].extra_code_h:
+        ##     _current_extra_code_h.extend(
+        ##         classes[code_obj.klass].extra_code_h[::-1])
+        ## if classes[code_obj.klass].extra_code_cpp:
+        ##     _current_extra_code_cpp.extend(
+        ##         classes[code_obj.klass].extra_code_h[::-1])
 
 
 def add_app(app_attrs, top_win_class):
