838ba5f971536ef317b93a0b666b6f381c8d0951
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2012-02-07 19:30:58 +0100

    Regorganise test code

--- tests/__init__.py
+++ tests/__init__.py
@@ -14,6 +14,7 @@ import unittest
 # import project modules
 import common
 import wxglade
+from xml_parse import CodeWriter
 
 
 class WXGladeBaseTest(unittest.TestCase):
@@ -78,7 +79,7 @@ class WXGladeBaseTest(unittest.TestCase):
 
         # replace original save_file() by test specific implementation
         self.origSaveFile = common.save_file
-        common.save_file = self.save_file
+        common.save_file = self._save_file
 
         # set own version string to prevent diff mismatches
         common.version = '"faked test version"'
@@ -89,44 +90,55 @@ class WXGladeBaseTest(unittest.TestCase):
             self.caseDirectory,
             )
 
-    def save_file(self, filename, content, which='wxg'):
+    def tearDown(self):
         """\
-        Test specific implementation of L{common.save_file()} to get the
-        result of the code generation without file creation.
-
-        The file content is stored in a StringIO instance. It's
-        accessible at L{self.vFiles} using the filename as key.
-
-        @note: The signature is as same as L{wxglade.common.save_file()} but
-               the functionality differs.
-
-        @param filename: Name of the file to create
-        @param content:  String to store into 'filename'
-        @param which:    Kind of backup: 'wxg' or 'codegen'
+        Cleanup
         """
-        self.failIf(
-            filename in self.vFiles,
-            "Virtual file %s already exists" % filename
-            )
-        self.failUnless(
-            filename,
-            "No filename given",
-            )
-        outfile = cStringIO.StringIO()
-        outfile.write(content)
-        self.vFiles[filename] = outfile
+        # cleanup virtual files
+        for filename in self.vFiles:
+            self.vFiles[filename].close()
+        self.vFiles = {}
 
-    def loadFile(self, casename, extension):
+        # restore original save_file() implementation
+        common.save_file = self.origSaveFile
+
+    def _generate_code(self, language, document, filename):
+        """\
+        Generate code for the given language.
+
+        @param language: Language to generate code for
+        @param document: XML document to generate code for
+        @type document:  String
+        @param filename: Name of the virtual output file
+        @type filename:  String
+        """
+        self.failUnless(
+            language in common.code_writers,
+            "No codewriter loaded for %s" % language
+            )
+
+        # generate code
+        CodeWriter(
+            writer=common.code_writers[language],
+            input=document,
+            from_string=True,
+            out_path=filename,
+            )
+
+        return
+
+    def _load_file(self, filename):
         """\
         Load a file need by a test case.
 
-        @param casename:  Name of the test case
-        @type casename:   String
+        @param filename:  Name of the file to load
+        @type filename:   String
         @param extension: File extension e.g. I{.wxg} or I{.py}
         @type extension:  String
         @return:          File content
         @rtype:           String
         """
+        casename, extension = os.path.splitext(filename)
         if extension == '.wxg':
             filetype = 'input'
         else:
@@ -160,7 +172,34 @@ class WXGladeBaseTest(unittest.TestCase):
 
         return content
 
-    def diff(self, text1, text2):
+    def _save_file(self, filename, content, which='wxg'):
+        """\
+        Test specific implementation of L{common.save_file()} to get the
+        result of the code generation without file creation.
+
+        The file content is stored in a StringIO instance. It's
+        accessible at L{self.vFiles} using the filename as key.
+
+        @note: The signature is as same as L{wxglade.common.save_file()} but
+               the functionality differs.
+
+        @param filename: Name of the file to create
+        @param content:  String to store into 'filename'
+        @param which:    Kind of backup: 'wxg' or 'codegen'
+        """
+        self.failIf(
+            filename in self.vFiles,
+            "Virtual file %s already exists" % filename
+            )
+        self.failUnless(
+            filename,
+            "No filename given",
+            )
+        outfile = cStringIO.StringIO()
+        outfile.write(content)
+        self.vFiles[filename] = outfile
+
+    def _diff(self, text1, text2):
         """\
         Compare two lists, tailing spaces will be removed
 
@@ -190,14 +229,50 @@ class WXGladeBaseTest(unittest.TestCase):
             )
         return '\n'.join(diff_gen)
 
-    def tearDown(self):
+    def _generate_and_compare(self, lang, inname, outname):
         """\
-        Cleanup
-        """
-        # cleanup virtual files
-        for filename in self.vFiles:
-            self.vFiles[filename].close()
-        self.vFiles = {}
+        Generate code and compare generated and expected code
 
-        # restore original save_file() implementation
-        common.save_file = self.origSaveFile
+        @param lang:    Language to generate code for
+        @type lang:     String
+        @param inname:  Name of the XML input file
+        @type inname:   String
+        @param outname: Name of the output file
+        @type outname:  String
+        """
+        # load XML input file
+        source = self._load_file(inname)
+        expected = self._load_file(outname)
+
+        # generate code
+        self._generate_code(lang, source, outname)
+        generated = self.vFiles[outname].getvalue()
+        self._compare(expected, generated)
+
+    def _compare(self, expected, generated, filetype=None):
+        """\
+        Compare expected and generated content using a diff algorithm
+
+        @param expected:  Expected content
+        @type expected:   Multiline String
+        @param generated: Generated content
+        @type generated:  Multiline String
+        @param filetype:  Short description of the content
+        @type filetype:   String
+        """
+        # compare files
+        delta = self._diff(expected, generated)
+
+        if filetype:
+            self.failIf(
+                delta,
+                "Generated %s file and expected result differs:\n%s" % (
+                    filetype.capitalize(),
+                    delta,
+                    )
+                )
+        else:
+            self.failIf(
+                delta,
+                "Generated file and expected result differs:\n%s"
+                )
