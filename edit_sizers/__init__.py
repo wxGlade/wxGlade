e598c81063f296c22a936c5e9d4a521bb5bd8018
Carsten Grohmann <mail@carstengrohmann.de>
2012-12-07 21:06:55 +0100

    Add some comments

--- edit_sizers/__init__.py
+++ edit_sizers/__init__.py
@@ -1,9 +1,29 @@
-# __init__.py: sizers module initialization
-# $Id: __init__.py,v 1.10 2007/03/27 07:02:06 agriggio Exp $
-#
-# Copyright (c) 2002-2007 Alberto Griggio <agriggio@users.sourceforge.net>
-# License: MIT (see license.txt)
-# THIS PROGRAM COMES WITH NO WARRANTY
+"""
+Sizers module initialization
+
+@copyright: 2002-2007 Alberto Griggio <agriggio@users.sourceforge.net>
+@license: MIT (see license.txt) - THIS PROGRAM COMES WITH NO WARRANTY
+"""
+
+Sizer = None
+"""\
+Shortcut for L{edit_sizers.Sizer}
+"""
+
+SizerSlot = None
+"""\
+Shortcut for L{edit_sizers.SizerSlot}
+"""
+
+SizerBase = None
+"""\
+Shortcut for L{edit_sizers.SizerBase}
+"""
+
+_builder = None
+"""\
+Shortcut for L{edit_sizers._builder}
+"""
 
 def init_all():
     import sizers_codegen
