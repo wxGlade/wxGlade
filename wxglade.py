795390fa717299905ce76067a3356bb7c6278b5e
Carsten Grohmann <mail@carstengrohmann.de>
2013-10-27 12:50:11 +0100

    Update copyright notice

--- wxglade.py
+++ wxglade.py
@@ -3,6 +3,7 @@
 Entry point of wxGlade
 
 @copyright: 2002-2007 Alberto Griggio <agriggio@users.sourceforge.net>
+@copyright: 2013 Carsten Grohmann
 @license: MIT (see license.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 """
 
