f36ded2bb13dbcb36d874c37c6997da4f35a1187
Carsten Grohmann <mail@carstengrohmann.de>
2014-11-09 21:41:02 +0100

    Show a status message after PyInstaller has run.

--- install/pyinstaller/build.bat
+++ install/pyinstaller/build.bat
@@ -1,10 +1,16 @@
+@echo off
 REM Simplify executing pyinstaller
 REM
 REM License: MIT (see license.txt)
 REM THIS PROGRAM COMES WITH NO WARRANTY
 
-pyinstaller --noconfirm ^
-            --clean ^
-            --onedir ^
+pyinstaller --noconfirm --clean --onedir ^
             --additional-hooks-dir=install/pyinstaller/hooks ^
             install\pyinstaller\wxglade.spec
+
+if errorlevel 1 (
+   echo PyInstaller finished with error code %errorlevel%
+   exit /b %errorlevel%
+) else (
+   echo PyInstaller finished successfully.
+)
