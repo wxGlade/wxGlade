9bebec1efd6ad8b6506141abe8e1dcf18effdced
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2014-12-07 16:11:17 +0100

    Show logged exceptions in wxGlade bug dialog

--- bugdialog.py
+++ bugdialog.py
@@ -20,28 +20,30 @@ class BugReport(bugdialog_ui.UIBugDialog):
     def __init__(self):
         bugdialog_ui.UIBugDialog.__init__(self, None, -1, "")
 
-    def SetContent(self, action=None, exc=None, ei=None):
+    def SetContent(self, action=None, exc=None, ei_msg=None, ei=None):
         """\
         Prepare given exception information and show it as dialog content.
 
-        Use parameters 'action' and 'exec' or just 'ei'.
+        Use parameters 'action' and 'exec' or 'msg' and 'ei'.
 
         @param action: Short description of the action that has raised this
                        error
         @type action:  str
         @param exc: Caught exception
         @type exc:  Exception
+        @param ei_msg: Short description of the exception
+        @type ei_msg:  str
         @param ei: Exception information
         @type ei: (exc_type, exc_value, exc_tb)
         """
-        assert (action and exc) or ei
+        assert (action and exc) or (ei_msg and ei)
 
         if exc:
             exc_type = exc.__class__.__name__
             exc_msg = str(exc)
         else:
             exc_type = ei[0]
-            exc_msg = None
+            exc_msg = ei_msg
 
         if not exc_msg:
             exc_msg = _('No summary available')
@@ -55,18 +57,21 @@ class BugReport(bugdialog_ui.UIBugDialog):
             'exc_type': exc_type,
             'exc_msg': exc_msg}
 
-        # deactivate the general msg dialog and reactivate it later again
+        # acquire lock to deactivate the general msg dialog
+        # otherwise the logging calls would trigger the dialog
+        # don't wait / fail if the log is already acquired - another function
+        # calling this dialog has acquired it
         app = wx.GetApp()
-        app.show_dialog = False
+        lock_acquire = app.lock_msgdialog.acquire(False)
 
         if exc:
             logging.exception(header)
         else:
             logging.error(header, exc_info=ei)
-
         details = log.getBufferAsString()
 
-        app.show_dialog = True
+        if lock_acquire:
+            app.lock_msgdialog.release()
 
         self.st_header.SetLabel(header)
         self.st_summary.SetLabel(summary)
