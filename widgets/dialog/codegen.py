b097163ee30af3a926edc1432c5c3a0935b9e606
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2015-04-07 18:53:24 +0200

    Add missing event types for event handling in C++

--- widgets/dialog/codegen.py
+++ widgets/dialog/codegen.py
@@ -2,7 +2,7 @@
 Code generator functions for wxDialog objects
 
 @copyright: 2002-2007 Alberto Griggio
-@copyright: 2014 Carsten Grohmann
+@copyright: 2014-2015 Carsten Grohmann
 @license: MIT (see license.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 """
 
@@ -103,6 +103,12 @@ class CppDialogGenerator(wcodegen.CppWidgetCodeWriter):
         except (KeyError, ValueError):
             pass
         return ret
+
+    def get_events(self, obj):
+        """\
+        wxDialog uses wxCloseEvent for event handling
+        """
+        return self.codegen.get_events_with_type(obj, 'wxCloseEvent')
 
 # end of class CppDialogGenerator
 
