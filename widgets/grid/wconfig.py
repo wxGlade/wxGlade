c907c761de631246feebb6f515482dde19999928
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2015-06-12 21:31:44 +0200

    Summarise event type definitions

--- widgets/grid/wconfig.py
+++ widgets/grid/wconfig.py
@@ -9,45 +9,22 @@ config = {
     'wxklass': 'wxGrid',
     'style_defs': {},
     'events': {
-        'EVT_TEXT_ENTER': {
+        'default': {
             'type': 'wxGridEvent',
         },
-        'EVT_GRID_CMD_CELL_LEFT_CLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_CELL_RIGHT_CLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_CELL_LEFT_DCLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_CELL_RIGHT_DCLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_LABEL_LEFT_CLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_LABEL_RIGHT_CLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_LABEL_LEFT_DCLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_LABEL_RIGHT_DCLICK': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_CELL_CHANGE': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_SELECT_CELL': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_EDITOR_HIDDEN': {
-            'type': 'wxGridEvent',
-        },
-        'EVT_GRID_CMD_EDITOR_SHOWN': {
-            'type': 'wxGridEvent',
-        },
+        'EVT_TEXT_ENTER': {},
+        'EVT_GRID_CMD_CELL_LEFT_CLICK': {},
+        'EVT_GRID_CMD_CELL_RIGHT_CLICK': {},
+        'EVT_GRID_CMD_CELL_LEFT_DCLICK': {},
+        'EVT_GRID_CMD_CELL_RIGHT_DCLICK': {},
+        'EVT_GRID_CMD_LABEL_LEFT_CLICK': {},
+        'EVT_GRID_CMD_LABEL_RIGHT_CLICK': {},
+        'EVT_GRID_CMD_LABEL_LEFT_DCLICK': {},
+        'EVT_GRID_CMD_LABEL_RIGHT_DCLICK': {},
+        'EVT_GRID_CMD_CELL_CHANGE': {},
+        'EVT_GRID_CMD_SELECT_CELL': {},
+        'EVT_GRID_CMD_EDITOR_HIDDEN': {},
+        'EVT_GRID_CMD_EDITOR_SHOWN': {},
         'EVT_GRID_CMD_COL_SIZE': {
             'type': 'wxGridSizeEvent',
         },
