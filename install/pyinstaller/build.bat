9f4f0022f223b21e0833e4d4a4d9af1db36b980b
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2015-08-04 18:45:49 +0200

    Update building Windows installer

--- install/pyinstaller/build.bat
+++ install/pyinstaller/build.bat
@@ -1,9 +1,36 @@
 @echo off
 REM Simplify executing pyinstaller
 REM
-REM License: MIT (see license.txt)
-REM THIS PROGRAM COMES WITH NO WARRANTY
+REM Copyright: 2014-2015 Carsten Grohmann
+REM License: MIT (see license.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 
+echo Check requirements:
+
+if not exist wxglade.py (
+  echo ERROR: The file "wxglade.py" does not exists!
+  echo        Please start this script from the base directory of the
+  echo        wxGlade source code only!
+  exit /b 1
+) else (
+  echo PASS:  Found wxglade.py
+)
+
+if not exist version.py (
+  echo WARN:  Version file "version.py" not found!
+  pause
+) else (
+  echo PASS:  Found version.py
+)
+
+echo Delete temporary Python bytecode files
+del /S *.pyo
+del /S *.pyc
+
+echo Delete temporary directories "dist" and "build"
+del /Q dist
+del /Q build
+
+echo Convert Python code into executable using PyInstaller
 pyinstaller --noconfirm --clean --onedir ^
             --additional-hooks-dir=install/pyinstaller/hooks ^
             install\pyinstaller\wxglade.spec
