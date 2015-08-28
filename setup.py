2283933a465a99776d3a999a9ea39bdbe8f62f6d
Carsten Grohmann <mail@carstengrohmann.de>
2015-08-28 20:15:46 +0200

    Rework generation of release packages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Setup script to create release packages
 @license: MIT (see license.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 import os
 from glob import glob
@@ -18,32 +18,39 @@ t.install()
 
 import config
 
+# Hack: use own implementation to filter module file "test.py"
+def find_package_modules2(self, package, package_dir):
+    self.check_package(package, package_dir)
+    module_files = glob(os.path.join(package_dir, "*.py"))
+    modules = []
+    setup_script = os.path.abspath(self.distribution.script_name)
 
-def is_package(path):
-    return os.path.isdir(path) and \
-        os.path.isfile(os.path.join(path, '__init__.py'))
+    for f in module_files:
+        abs_f = os.path.abspath(f)
+        if package == 'wxglade' and os.path.normpath(f) == 'test.py':
+            self.debug_print("excluding %s" % f)
+        elif abs_f != setup_script:
+            module = os.path.splitext(os.path.basename(f))[0]
+            modules.append((package, module, f))
+        else:
+            self.debug_print("excluding %s" % setup_script)
+    return modules
 
+# inject own implementation
+from distutils.command.build_py import build_py
+build_py.find_package_modules = find_package_modules2
 
-def find_packages(path, base=""):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package(dir):
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
 
 classifiers = """\
 Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Natural Language :: English
-Operating System :: OS Independent (Written in an interpreted language)
+Operating System :: OS Independent
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
 Programming Language :: Python
 Programming Language :: Python :: 2
 Programming Language :: Python :: 2.4
@@ -65,39 +72,29 @@ wxPython, that helps you create wxWidgets/wxPython user interfaces. At
 the moment it can generate Python, C++, Perl, Lisp and XRC (wxWidgets'
 XML resources) code."""
 
-text_files = ['CHANGES.txt', 'CONTRIBUTING.txt', 'credits.txt', 'epydoc.conf',
-              'license.txt', 'Makefile', 'NEWS.txt', 'README.txt',
-              'README.SF.md', 'TODO.txt', 'appdata.xml', 'wxGlade.desktop']
+text_files = ['CHANGES.txt', 'CONTRIBUTING.txt', 'credits.txt',
+              'license.txt', 'NEWS.txt', 'README.txt', 'TODO.txt', ]
 
 data_files = [
     ['share/wxglade/icons', glob('icons/*.*')],
     ['share/wxglade/icons/gtk', glob('icons/gtk/*')],
     ['share/wxglade/icons/msw', glob('icons/msw/*')],
+    ['share/wxglade/templates', glob('templates/*')],
     ['share/doc/wxglade', text_files],
-    ['share/doc/wxglade/doc', glob('docs/*.html')],
-    ['share/doc/wxglade/doc/img', glob('docs/img/*.*')],
-    ['share/doc/wxglade/doc', glob('docs/*.txt')],
-    ['share/doc/wxglade/doc/html', glob('docs/html/*.*')],
-    ['share/doc/wxglade/doc/pdf', glob('docs/pdf/*.pdf')],
+    ['share/doc/wxglade/tutorial', glob('docs/tutorial.html')],
+    ['share/doc/wxglade/tutorial/img', glob('docs/img/*.*')],
+    ['share/doc/wxglade/manual_html', glob('docs/html/*.*')],
+    ['share/doc/wxglade/manual_pdf', glob('docs/pdf/*.pdf')],
     ['share/man/man1', ['docs/man/wxglade.1']],
-    ['share/doc/wxglade', ['docs/man/manpage.xml']],
-    ['share/doc/wxglade', ['docs/src/manual.xml']],
-    ['share/doc/wxglade/install', glob('install/*.*')],
-    ['share/doc/wxglade/install/rpm', glob('install/rpm/*.*')],
-    ['share/doc/wxglade/install/pyinstaller',
-     glob('install/pyinstaller/*.*')],
 ]
 
-scripts = ['wxglade', 'wxglade.pyw']
-
-packages = find_packages(path=".", base='wxglade').keys()
+packages = ['wxglade.%s' % pkg for pkg in find_packages(exclude=['tests'])]
 packages.append('wxglade')
 
 # write and handle version file
 version = config.get_version(False)
 if not os.path.exists('version.py'):
     config.write_version_file(version)
-text_files.append('version.py')
 
 setup(
     name='wxGlade',
@@ -112,13 +109,12 @@ setup(
     long_description=long_description,
     license='MIT License',
     platforms=['WIN32', 'OSX', 'POSIX'],
-    scripts=scripts,
+    scripts=['wxglade'],
     packages=packages,
     package_dir={'wxglade': '.'},
     package_data={'wxglade.widgets': ['widgets.txt'],
-                  'wxglade.tests': ['casefiles/*.*'],
                   'wxglade': ['res/*.*']},
     data_files=data_files,
     install_requires=['wxPython >=2.8'],
     setup_requires=["setuptools_hg"],
-    )
+)
