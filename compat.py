bc3f67a0836f1a4e64d63538f8b6051e6d097a15
Carsten Grohmann <CarstenGrohmann@users.noreply.github.com>
2015-11-25 20:35:54 +0100

    Rework handling of compat function
    Take the decision which function is to use once per application runtime
    and not once per function call

--- compat.py
+++ compat.py
@@ -8,15 +8,51 @@ Compatibility code to run with different versions of wxPython
 import wx
 
 
-def SizerItem_SetWindow(item, window):
+SizerItem_SetWindow = None
+
+
+def SizerItem_AssignWindow(item, window):
     """\
+    Wrapper for wx.SizerItem.SetWindow() resp. wx.SizerItem.AssignWindow()
+
     wx.SizerItem.SetWindow() is deprecated since wxPython 2.9 use
     wx.SizerItem.AssignWindow() instead.
 
+    Depending on the wxPython version L{SizerItem_SetWindow28()} or
+    L{SizerItem_AssignWindow()} will be used.
+
     @param item:   Instance of wx.SizerItem
     @param window: Instance of wx.Window
+
+    @see: L{SizerItem_SetWindow()}
+    @see: L{SizerItem_SetWindow28()}
+    @see: L{SizerItem_AssignWindow()}
     """
-    if wx.VERSION[:2] >= (2, 9):
-        item.AssignWindow(window)
-    else:
-        item.SetWindow(window)
+    item.AssignWindow(window)
+
+
+def SizerItem_SetWindow28(item, window):
+    """\
+    Wrapper for wx.SizerItem.SetWindow() resp. wx.SizerItem.AssignWindow()
+
+    wx.SizerItem.SetWindow() is deprecated since wxPython 2.9 use
+    wx.SizerItem.AssignWindow() instead.
+
+    Depending on the wxPython version L{SizerItem_SetWindow28()} or
+    L{SizerItem_AssignWindow()} will be used.
+
+    @param item:   Instance of wx.SizerItem
+    @param window: Instance of wx.Window
+
+    @see: L{SizerItem_SetWindow()}
+    @see: L{SizerItem_SetWindow28()}
+    @see: L{SizerItem_AssignWindow()}
+    """
+    item.SetWindow(window)
+
+
+# Set different functions depending on the active wxPython version
+if wx.VERSION[:2] >= (2, 9):
+    SizerItem_SetWindow = SizerItem_AssignWindow
+else:
+    SizerItem_SetWindow = SizerItem_SetWindow28
