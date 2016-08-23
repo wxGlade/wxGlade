cb762da09f157ecaaf251769c8f458b007379762
Carsten Grohmann <mail@carstengrohmann.de>
2016-08-23 19:46:38 +0200

    Add excludes for wxCheckListBox mutual styles
    The styles wxLB_SINGLE, wxLB_MULTIPLE and wxLB_EXTENDED are mutual and
    should exclude the other automatically.
    (grafted from 640b37e3895204213f41896395e6ef28f93c6550)

--- widgets/check_list_box/wconfig.py
+++ widgets/check_list_box/wconfig.py
@@ -11,16 +11,19 @@ config = {
     'style_defs': {
         'wxLB_SINGLE': {
             'desc': _('Single-selection list.'),
+            'exclude': 'wxLB_EXTENDED|wxLB_MULTIPLE',
         },
         'wxLB_MULTIPLE': {
             'desc': _('Multiple-selection list: the user can toggle multiple '
                       'items on and off. This is the same as wxLB_EXTENDED '
                       'in wxGTK2 port.'),
+            'exclude': 'wxLB_EXTENDED|wxLB_SINGLE',
         },
         'wxLB_EXTENDED': {
             'desc': _('Extended-selection list: the user can extend the '
                       'selection by using SHIFT or CTRL keys together with '
                       'the cursor movement keys or the mouse.'),
+            'exclude': 'wxLB_SINGLE|wxLB_MULTIPLE',
         },
         'wxLB_HSCROLL': {
             'desc': _('Create horizontal scrollbar if contents are too wide '
