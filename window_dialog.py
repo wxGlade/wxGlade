00820693d2c1021a5647ca625b62e67fd013fc20
atlassian <atlassian@schwertberger.de>
2017-06-16 21:40:41 +0200

    usability improvement: ask user whether he really wants a standalone menu/tool bar

--- window_dialog.py
+++ window_dialog.py
@@ -1,6 +1,6 @@
 """Dialog to ask user for base class and class name
 
-@copyright: 2016 Dietmar Schwertberger
+@copyright: 2016-2017 Dietmar Schwertberger
 @license: MIT (see LICENSE.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 """
 
@@ -11,16 +11,17 @@ import common, compat
 
 class WindowDialog(wx.Dialog):
     # dialog for builder function
+    parent = parent_property = None  # used by StandaloneOrChildDialog
     def __init__(self, klass, base_classes=None, message="Select Class", toplevel=True):
-        global _NUMBER
         wx.Dialog.__init__(self, None, -1, _(message))
+
+        self.standalone = self.base = None
 
         if base_classes:
             # base class
             self.base  = wx.RadioBox(self, -1, ("base class"), choices=base_classes, style=wx.RA_VERTICAL)
             self.base.SetSelection(0)
-        else:
-            self.base = None
+
         self.number = 1
         self.class_names = set( common.app_tree.get_all_class_names() )
         self.toplevel = toplevel  # if this is True, the name must be unique, as the generated class will have it
@@ -30,10 +31,25 @@ class WindowDialog(wx.Dialog):
             klass = self.get_next_class_name(klass)
         self.klass = wx.TextCtrl(self, -1, klass)
         self.klass.Bind(wx.EVT_TEXT, self.on_text)  # for validation
+
+        # for frame or standalone? this is used by the derived class below and just for menu bar and tool bar
+        if self.parent_property and self.parent:
+            # self.parent must be set by the derived class in this case; here we check whether it is set already
+            choices = ["Add to %s '%s'"%(self.parent.base[2:], self.parent.name),
+                       "Standalone"]
+            self.standalone  = wx.RadioBox(self, -1, ("Type"), choices=choices, style=wx.RA_VERTICAL)
+            self.standalone.Bind(wx.EVT_RADIOBOX, self.on_standalone)
+            if self.parent.properties[self.parent_property].value:
+                self.standalone.SetSelection(1)
+                self.standalone.Disable()
+            else:
+                self.standalone.SetSelection(0)
+                self.klass.Disable()
+
         # layout
         szr = wx.BoxSizer(wx.VERTICAL)
-        if self.base:
-            szr.Add(self.base, 0, wx.ALL|wx.EXPAND, 5)
+        if self.standalone: szr.Add(self.standalone, 0, wx.ALL|wx.EXPAND, 5)
+        if self.base:       szr.Add(self.base,       0, wx.ALL|wx.EXPAND, 5)
         hszr = wx.BoxSizer(wx.HORIZONTAL)
         hszr.Add(wx.StaticText(self, -1, _("class"),), 0, wx.ALIGN_CENTRE_VERTICAL|wx.ALIGN_RIGHT|wx.EXPAND|wx.ALL, 3)
         hszr.Add(self.klass, 2)
@@ -91,13 +107,27 @@ class WindowDialog(wx.Dialog):
         self.btnOK.Enable(OK)
         event.Skip()
 
+    def on_standalone(self, event):
+        self.klass.Enable(event.Selection==1)
+        event.Skip()
+
     def show(self):
         "returns values if user has hit OK, None otherwise"
         res  = self.ShowModal()
         if res != wx.ID_OK: return None
 
+        if self.standalone and self.standalone.GetSelection()==0:
+            return True
         klass  = self.klass.GetValue()
         if not self.base:
             return klass
         base = self.base.GetStringSelection()
         return klass, base
+
+
+class StandaloneOrChildDialog(WindowDialog):
+    # for menu and tool bars
+    def __init__(self, klass, message, parent, parent_property):
+        self.parent = parent
+        self.parent_property = parent_property
+        WindowDialog.__init__(self, klass, None, message, True)
