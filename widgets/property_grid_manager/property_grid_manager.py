531a6d4fa6afa326db25d117ceb19bb798b8a4a3
DietmarSchwertberger <atlassian@schwertberger.de>
2017-08-10 00:16:31 +0200

    property grid manager: usability improvements (example data, styles)

--- widgets/property_grid_manager/property_grid_manager.py
+++ widgets/property_grid_manager/property_grid_manager.py
@@ -25,17 +25,81 @@ class EditPropertyGridManager(ManagedBase, EditStylesMixin):
         EditStylesMixin.__init__(self)
 
     def create_widget(self):
-        if compat.IS_CLASSIC:
-            self.widget = PropertyGridManager(self.parent.widget, self.id, (200, 200))
-            # following two events are to permit select grid from designer frame
-            self.widget.Bind(EVT_PG_SELECTED, self.on_set_focus)
-            # these are to show the popup menu on right click
-            self.widget.Bind(EVT_PG_RIGHT_CLICK, self.popup_menu)
-        else:
-            # workaround: this is not yet working on Phoenix
-            self.widget = wx.Panel(self.parent.widget, self.id, (200, 200))
-            self.widget.Bind(wx.EVT_LEFT_DOWN, self.on_set_focus)
-            self.widget.SetToolTip("PropertyGridManager crashes on Phoenix, so you see just a panel")
+        self.widget = PropertyGridManager(self.parent.widget, self.id, (200,200), style=self.style)
+
+        # property grid does handle left and right mouse clicks and emits these events:
+        self.widget.Bind(EVT_PG_SELECTED, self.on_set_focus)
+        self.widget.Bind(EVT_PG_RIGHT_CLICK, self.on_right_click)
+
+        self._create_dummy_content()
+
+    def on_right_click(self, event):
+        self.popup_menu( event, self.widget.ScreenToClient( wx.GetMousePosition() ) )
+
+    def _create_dummy_content(self):
+        # some dummy content from the wxPython demo
+        import wx.propgrid as wxpg
+        pg = self.widget
+        pg.AddPage( "Page 1 - Dummy" )
+
+        pg.Append( wxpg.PropertyCategory("1 - Basic Properties") )
+        pg.Append( wxpg.StringProperty("String",value="Some Text") )
+
+        sp = pg.Append( wxpg.StringProperty('StringProperty w/ Password flag', value='ABadPassword') )
+        sp.SetAttribute('Hint', 'This is a hint')
+        sp.SetAttribute('Password', True)
+
+        pg.Append( wxpg.IntProperty("Int",value=123) )
+        pg.Append( wxpg.FloatProperty("Float",value=123.4) )
+        pg.Append( wxpg.BoolProperty("Bool",value=True) )
+        boolprop = pg.Append( wxpg.BoolProperty("Bool_with_Checkbox",value=True) )
+        pg.SetPropertyAttribute( "Bool_with_Checkbox", "UseCheckbox", True)
+        pg.Append( wxpg.PropertyCategory("2 - More Properties") )
+        pg.Append( wxpg.LongStringProperty("LongString", value="This is a\nmulti-line string\nwith\ttabs\nmixed\tin."))
+        pg.Append( wxpg.DirProperty("Dir",value=r"C:\Windows") )
+        pg.Append( wxpg.FileProperty("File",value=r"C:\Windows\system.ini") )
+        pg.Append( wxpg.ArrayStringProperty("ArrayString",value=['A','B','C']) )
+
+        pg.Append( wxpg.EnumProperty("Enum","Enum", ['wxPython Rules', 'wxPython Rocks', 'wxPython Is The Best'],
+                                                    [10,11,12], 0) )
+        pg.Append( wxpg.EditEnumProperty("EditEnum","EditEnumProperty", ['A','B','C'], [0,1,2], "Text Not in List") )
+
+        pg.Append( wxpg.PropertyCategory("3 - Advanced Properties") )
+        pg.Append( wxpg.DateProperty("Date",value=wx.DateTime.Now()) )
+        pg.Append( wxpg.FontProperty("Font",value=self.widget.GetFont()) )
+        pg.Append( wxpg.ColourProperty("Colour", value=self.widget.GetBackgroundColour()) )
+        pg.Append( wxpg.SystemColourProperty("SystemColour") )
+        pg.Append( wxpg.ImageFileProperty("ImageFile") )
+        pg.Append( wxpg.MultiChoiceProperty("MultiChoice", choices=['wxWidgets','QT','GTK+']) )
+
+        pg.Append( wxpg.PropertyCategory("4 - Additional Properties") )
+        pg.Append( wxpg.IntProperty("IntWithSpin",value=256) )
+        pg.SetPropertyEditor("IntWithSpin","SpinCtrl")
+
+        pg.SetPropertyAttribute( "File", wxpg.PG_FILE_SHOW_FULL_PATH, 0 )
+        pg.SetPropertyAttribute( "File", wxpg.PG_FILE_INITIAL_PATH, r"C:\Program Files\Internet Explorer" )
+        pg.SetPropertyAttribute( "Date", wxpg.PG_DATE_PICKER_STYLE, wx.adv.DP_DROPDOWN|wx.adv.DP_SHOWCENTURY )
+
+        pg.AddPage( "Page 2 - Almost Empty" )
+        pg.Append( wxpg.PropertyCategory("1 - Basic Properties") )
+        pg.Append( wxpg.StringProperty("String 2",value="Some Text") )
+
+
+    def _set_widget_style(self):
+        "re-create widget on style changes"
+        if not self.widget: return
+        old_style = self.widget.GetWindowStyleFlag()
+        new_style = self.style
+        if old_style == new_style: return
+        self.widget.Hide()
+        #sizer_item = self.sizer.widget.GetItem(self.widget)
+        #sizer_item.AssignWindow(None)
+        #if self.sizer and not self.sizer.is_virtual(): self.sizer.widget.Detach(self.widget)
+        old_widget = self.widget
+        self.widget = None
+        self.create_widget()
+        self.finish_widget_creation()
+        if old_widget: compat.DestroyLater(old_widget)  # it may have been destroyed already, e.g. by a virtual sizer
 
     def get_property_handler(self, name):
         return ManagedBase.get_property_handler(self, name)
