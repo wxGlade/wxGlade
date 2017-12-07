eec868ba711af59e123898180e66a7abe30a865f
Dietmar Schwertberger <5512021+DietmarSchwertberger@users.noreply.github.com>
2017-12-07 22:33:26 +0100

    increase robustness

--- widgets/radio_box/radio_box.py
+++ widgets/radio_box/radio_box.py
@@ -112,7 +112,7 @@ class EditRadioBox(ManagedBase):
     def _set_choices(self):
         if not self.widget: return
         for b in self.buttons or []:
-            compat.DestroyLater(b)
+            if b: compat.DestroyLater(b)
         self.static_box = self._create_static_box()
         self.buttons = [self._create_button(c) for (c,) in self.choices]
         self._do_layout()
