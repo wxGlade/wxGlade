ddd0ff895ba4ec320a01b6c5ef75d52cfb9c247d
Dietmar Schwertberger <5512021+DietmarSchwertberger@users.noreply.github.com>
2017-12-09 16:10:25 +0100

    update About dialog

--- about.py
+++ about.py
@@ -3,6 +3,7 @@ About box with general info
 
 @copyright: 2002-2007 Alberto Griggio
 @copyright: 2014-2016 Carsten Grohmann
+@copyright: 2017 Dietmar Schwertberger
 @license: MIT (see LICENSE.txt) - THIS PROGRAM COMES WITH NO WARRANTY
 """
 
@@ -22,13 +23,12 @@ class wxGladeAboutBox(wx.Dialog):
     <body bgcolor="%s">
     <!-- <font size="-1"> -->
     <center>
-    <table align="center" width="380" border="2" cellspacing="0">
+    <table align="center" border="2" cellspacing="0">
     <tr>
     <td align="center"><img src="%s">
     </td></tr>
     <tr><td bgcolor="#000000" align="center">
-    <font color="#ffffff">Version %s on Python %s and wxPython %s
-    </font>
+    <font color="#ffffff">Version %s on Python %s and wxPython %s</font>
     </td></tr>
     </table>
     </center>
@@ -50,50 +50,8 @@ class wxGladeAboutBox(wx.Dialog):
 
     def __init__(self, parent=None):
         wx.Dialog.__init__(self, parent, -1, _('About wxGlade'))
-
-        class HtmlWin(wx.html.HtmlWindow):
-            def OnLinkClicked(self, linkinfo):
-                href = linkinfo.GetHref()
-                if href == 'show_license':
-                    if config.license_file:
-                        from wx.lib.dialogs import ScrolledMessageDialog
-                        try:
-                            license_file = codecs.open(
-                                config.license_file, encoding='UTF-8')
-                            dlg = ScrolledMessageDialog(
-                                self,
-                                license_file.read(),
-                                _("wxGlade - License")
-                                )
-                            license_file.close()
-                            dlg.ShowModal()
-                            dlg.Destroy()
-                        except EnvironmentError as inst:
-                            bugdialog.ShowEnvironmentError(
-                                _('''Can't read the file "LICENSE.txt".\n\nYou can get a license copy at\n'''
-                                  '''http://www.opensource.org/licenses/mit-license.php'''), inst)
-                    else:
-                        wx.MessageBox(_('File "LICENSE.txt" not found!\nYou can get a license copy at\n'
-                                        'http://www.opensource.org/licenses/mit-license.php'),
-                                      _('Error'), wx.OK | wx.CENTRE | wx.ICON_EXCLAMATION)
-                elif href == 'show_credits':
-                    if config.credits_file:
-                        from wx.lib.dialogs import ScrolledMessageDialog
-                        try:
-                            credits_file = codecs.open( config.credits_file, encoding='UTF-8' )
-                            dlg = ScrolledMessageDialog( self, credits_file.read(), _("wxGlade - Credits") )
-                            credits_file.close()
-                            dlg.ShowModal()
-                            dlg.Destroy()
-                        except EnvironmentError as inst:
-                            bugdialog.ShowEnvironmentError(_('''Can't read the file "CREDITS.txt"'''), inst)
-                    else:
-                        wx.MessageBox(_('File "CREDITS.txt" not found!'), _('Error'),
-                                      wx.OK | wx.CENTRE | wx.ICON_EXCLAMATION)
-                else:
-                    import webbrowser
-                    webbrowser.open(linkinfo.GetHref(), new=True)
-        html = HtmlWin(self, -1, size=(400, 250))
+        html = wx.html.HtmlWindow(self, -1, size=(480, 250))
+        html.Bind(wx.html.EVT_HTML_LINK_CLICKED, self.OnLinkClicked)
         # it's recommended at least for GTK2 based wxPython
         if "gtk2" in wx.PlatformInfo:
             html.SetStandardFonts()
@@ -112,7 +70,8 @@ class wxGladeAboutBox(wx.Dialog):
         szr2.Add(btn)
         if wx.Platform == '__WXGTK__':
             extra_border = 5  # border around a default button
-        else: extra_border = 0
+        else:
+            extra_border = 0
         szr.Add(szr2, 0, wx.ALL|wx.ALIGN_RIGHT, 20 + extra_border)
         self.SetAutoLayout(True)
         self.SetSizer(szr)
@@ -120,4 +79,42 @@ class wxGladeAboutBox(wx.Dialog):
         self.Layout()
         if parent: self.CenterOnParent()
         else: self.CenterOnScreen()
+
+    def OnLinkClicked(self, event):
+        href = event.GetLinkInfo().GetHref()
+        if href == 'show_license':
+            if config.license_file:
+                from wx.lib.dialogs import ScrolledMessageDialog
+                try:
+                    license_file = codecs.open(config.license_file, encoding='UTF-8')
+                    dlg = ScrolledMessageDialog( self, license_file.read(), "wxGlade - License" )
+                    license_file.close()
+                    dlg.ShowModal()
+                    dlg.Destroy()
+                except EnvironmentError as inst:
+                    bugdialog.ShowEnvironmentError(
+                        _('''Can't read the file "LICENSE.txt".\n\nYou can get a license copy at\n'''
+                          '''http://www.opensource.org/licenses/mit-license.php'''), inst)
+            else:
+                wx.MessageBox(_('File "LICENSE.txt" not found!\nYou can get a license copy at\n'
+                                'http://www.opensource.org/licenses/mit-license.php'),
+                              _('Error'), wx.OK | wx.CENTRE | wx.ICON_EXCLAMATION)
+        elif href == 'show_credits':
+            if config.credits_file:
+                from wx.lib.dialogs import ScrolledMessageDialog
+                try:
+                    credits_file = codecs.open( config.credits_file, encoding='UTF-8' )
+                    dlg = ScrolledMessageDialog( self, credits_file.read(), _("wxGlade - Credits") )
+                    credits_file.close()
+                    dlg.ShowModal()
+                    dlg.Destroy()
+                except EnvironmentError as inst:
+                    bugdialog.ShowEnvironmentError(_('''Can't read the file "CREDITS.txt"'''), inst)
+            else:
+                wx.MessageBox(_('File "CREDITS.txt" not found!'), _('Error'),
+                              wx.OK | wx.CENTRE | wx.ICON_EXCLAMATION)
+        else:
+            import webbrowser
+            webbrowser.open(href, new=True)
+
 
