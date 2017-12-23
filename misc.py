47c01e94425f1d13a04f1778c0b937085640ad24
DietmarSchwertberger <atlassian@schwertberger.de>
2017-12-23 21:12:02 +0100

    enable correct notebook page selection if base is changed

--- misc.py
+++ misc.py
@@ -45,8 +45,8 @@ def show_widget(widget):
     while True:
         if not widget.node or not widget.node.parent: break  # Application.node is None
         parent = widget.node.parent.widget
-        if parent.klass=="wxNotebook":
-            # a wiget under a wxNotebook without a panel: select page
+        if parent.__class__.__name__=="EditNotebook":
+            # a widget under a wxNotebook without a panel: select page
             if parent.widget and widget in parent.pages:
                 parent.widget.SetSelection( parent.pages.index(widget) )
 
