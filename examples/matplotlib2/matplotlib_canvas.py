281d0c2e3aa4824cacf9cef8c60648788da7e397
DietmarSchwertberger <atlassian@schwertberger.de>
2018-01-01 17:40:04 +0100

    add example matplotlib2

---
+++ examples/matplotlib2/matplotlib_canvas.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# a class to make matplotib.backend_wxagg.FigureCanvasWxAgg compatible to wxGlade
+
+import wx
+
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+
+
+class MatplotlibCanvas(FigureCanvas):
+    def __init__(self, parent, id=wx.ID_ANY):
+        # 1x1 grid, first subplot
+        figure = self.figure = Figure()
+        self.axes = figure.add_subplot(111)
+        FigureCanvas.__init__(self, parent, id, figure)
+
+    def clear(self):
+        self.axes.clear()
+        self.draw()
---
+++ examples/matplotlib2/matplotlib_example.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+
+import wx
+import matplotlib_GUI
+
+
+class MyFrame(matplotlib_GUI.MyFrame):
+
+    def on_button_plot(self, event):
+        import numpy
+        xmin = xmax = step = None
+        try:
+            xmin = float( self.text_xmin.GetValue() )
+            self.text_xmin.SetBackgroundColour(wx.WHITE)
+        except:
+            self.text_xmin.SetBackgroundColour(wx.RED)
+        try:
+            xmax = float( self.text_max.GetValue() )
+            self.text_max.SetBackgroundColour(wx.WHITE)
+        except:
+            self.text_max.SetBackgroundColour(wx.RED)
+        try:
+            step = float( self.text_xstep.GetValue() )
+            self.text_xstep.SetBackgroundColour(wx.WHITE)
+        except:
+            self.text_xstep.SetBackgroundColour(wx.RED)
+            
+        x = numpy.arange(xmin, xmax, step)
+        # build globals with some functions
+        g = {}
+        for name in ["sin","cos","tan","ufunc","square"]:
+            g[name] = getattr(numpy, name)
+        y = eval(self.text_function.GetValue(), g, {"numpy":numpy, "x":x})
+        self.matplotlib_canvas.axes.plot(x,y)
+        self.matplotlib_canvas.draw()
+        event.Skip()
+
+    def on_button_clear(self, event):
+        self.matplotlib_canvas.clear()
+        event.Skip()
+
+
+class MyApp(wx.App):
+    def OnInit(self):
+        self.frame = MyFrame(None, wx.ID_ANY, "")
+        self.SetTopWindow(self.frame)
+        self.frame.Show()
+        return True
+
+
+if __name__ == "__main__":
+    app = MyApp(0)
+    app.MainLoop()
