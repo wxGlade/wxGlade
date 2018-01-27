02d4422ae41b0d28d88ffbda67cea8620c63aa73
DietmarSchwertberger <atlassian@schwertberger.de>
2018-01-27 19:19:10 +0100

    display widget structure and layout for debugging (by Shift-Clicking on "Show Preview" or "Show Design Window")

---
+++ utilities.py
@@ -0,0 +1,68 @@
+"""
+Utilities, e.g. for debugging
+
+@copyright: 2016-2018 Dietmar Schwertberger
+@license: MIT (see LICENSE.txt) - THIS PROGRAM COMES WITH NO WARRANTY
+"""
+
+
+from __future__ import print_function
+
+import wx
+
+class StructurePrinter:
+    # print the structure and sizes of a window with all it's children
+    def __init__(self, window):
+        self.window(window, 0)
+
+    def _sizer_item(self, si, indent):
+        expand = (si.GetFlag() & wx.EXPAND) and "EXPAND" or ""
+        print( "  "*indent, " SI:", si.GetMinSize(), " ", si.GetSize(), " ", si.GetProportion(), expand )
+
+    def sizer(self, sizer, si, indent=0):
+        cname = sizer.GetClassName() # u'wxPanel'
+        print( "  "*indent, "%s"%cname, sizer.GetSize(), sizer )
+
+        if si: self._sizer_item(si, indent)
+        print()
+
+        # list all by sizer
+        for si in sizer.GetChildren():
+            child = si.GetWindow()
+            if child is not None:
+                self.window(child, si, indent+1)
+                continue
+            child = si.GetSizer()
+            if child is not None:
+                self.sizer(child, si, indent+1)
+                continue
+            child = si.GetSpacer()
+            if child is not None:
+                self.spacer(child, si, indent+1)
+                continue
+            raise ValueError("XXX")
+
+    def window(self, widget, si, indent=0):
+        cname = widget.GetClassName() # u'wxPanel'
+        name  = widget.GetName() # u'panel'
+        print( "  "*indent, "%s: %s"%(cname, name), widget.GetSize(), widget.GetBestSize(), widget.GetEffectiveMinSize() )
+
+        if si: self._sizer_item(si, indent)
+
+        sizer = widget.GetSizer()
+
+        if sizer:
+            # list all by sizer
+            self.sizer(sizer, None, indent+1)
+        else:
+            # list all children
+            for child in widget.GetChildren():
+                self.window(child, None, indent+1)
+        print()
+
+    def spacer(self, spacer, si, indent=0):
+        # a spacer
+        print( "  "*indent, "Spacer", spacer.Get() )
+        self._sizer_item(si, indent)
+        print()
+
