7e00ab0a9a6c66d60a8e89048bb94869a5f693bf
DietmarSchwertberger <github@schwertberger.de>
2018-05-30 00:02:23 +0200

    set toggle_button label on creation

--- widgets/toggle_button/toggle_button.py
+++ widgets/toggle_button/toggle_button.py
@@ -30,7 +30,7 @@ class EditToggleButton(ManagedBase, EditStylesMixin, BitmapMixin):
         EditStylesMixin.__init__(self)
 
         # initialise instance variable
-        self.label = np.TextProperty("", multiline="grow")
+        self.label = np.TextProperty(label, multiline="grow")
         self.value = np.CheckBoxProperty(False, default_value=False)
         # bitmaps are only for >= 3.0
         self.bitmap          = np.BitmapPropertyD(min_version=(3,0))
@@ -63,12 +63,12 @@ class EditToggleButton(ManagedBase, EditStylesMixin, BitmapMixin):
 
 def builder(parent, sizer, pos, number=[1]):
     "factory function for EditToggleButton objects"
-    label = u'button_%d' % number[0]
-    while common.app_tree.has_name(label):
+    name = u'button_%d' % number[0]
+    while common.app_tree.has_name(name):
         number[0] += 1
-        label = u'button_%d' % number[0]
+        name = u'button_%d' % number[0]
     with parent.frozen():
-        button = EditToggleButton(label, parent, wx.NewId(), label, sizer, pos)
+        button = EditToggleButton(name, parent, wx.NewId(), name, sizer, pos)
         button.properties["style"].set_to_default()
         button.check_defaults()
         node = Node(button)
@@ -81,12 +81,12 @@ def xml_builder(attrs, parent, sizer, sizeritem, pos=None):
     "factory to build EditToggleButton objects from a XML file"
     from xml_parse import XmlParsingError
     try:
-        label = attrs['name']
+        name = attrs['name']
     except KeyError:
         raise XmlParsingError(_("'name' attribute missing"))
     if sizer is None or sizeritem is None:
         raise XmlParsingError(_("sizer or sizeritem object cannot be None"))
-    button = EditToggleButton(label, parent, wx.NewId(), '', sizer, pos)
+    button = EditToggleButton(name, parent, wx.NewId(), '', sizer, pos)
     #sizer.set_item(button.pos, proportion=sizeritem.proportion, span=sizeritem.span, flag=sizeritem.flag, border=sizeritem.border)
     node = Node(button)
     button.node = node
