d676d0d4bab0b8f659cde66d4e1bec342edf20fc
DietmarSchwertberger <github@schwertberger.de>
2020-01-25 18:20:33 +0100

    avoid assertion error with MDI child frames

--- application.py
+++ application.py
@@ -596,6 +596,7 @@ class Application(EditRoot):
 
             if issubclass(preview_class, wx.MDIChildFrame):
                 frame = wx.MDIParentFrame(None, -1, '')
+                frame.SetMenuBar( wx.MenuBar() )  # avoid assertion error
                 child = preview_class(frame, -1, '')
                 child.SetTitle('<Preview> - ' + child.GetTitle())
                 w, h = child.GetSize()
