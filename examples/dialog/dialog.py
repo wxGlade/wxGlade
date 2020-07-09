5b0d501e70952f84360aaee36414cf11cace17c6
DietmarSchwertberger <github@schwertberger.de>
2020-07-09 22:15:47 +0200

    extend dialog example to open from a menu as well

--- examples/dialog/dialog.py
+++ examples/dialog/dialog.py
@@ -20,28 +20,38 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, *args, **kwds)
         self.SetSize((400, 300))
         self.SetTitle("frame")
-        
+
+        # Menu Bar
+        self.frame_menubar = wx.MenuBar()
+        wxglade_tmp_menu = wx.Menu()
+        item = wxglade_tmp_menu.Append(wx.ID_ANY, "Show Dialog", "")
+        self.Bind(wx.EVT_MENU, self.show_modal, item)
+        self.frame_menubar.Append(wxglade_tmp_menu, "Menu")
+        self.SetMenuBar(self.frame_menubar)
+        # Menu Bar end
+
         self.panel_1 = wx.Panel(self, wx.ID_ANY)
-        
+
         sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        
+
         self.button_1 = wx.Button(self.panel_1, wx.ID_ANY, "Show Dialog (modal)")
         sizer_1.Add(self.button_1, 0, wx.ALL, 4)
-        
+
         static_text_1 = wx.StaticText(self.panel_1, wx.ID_ANY, "Text from dialog:")
         sizer_1.Add(static_text_1, 0, wx.ALL, 4)
-        
+
         self.text_ctrl_1 = wx.TextCtrl(self.panel_1, wx.ID_ANY, "", style=wx.TE_READONLY)
         sizer_1.Add(self.text_ctrl_1, 0, wx.ALL | wx.EXPAND, 4)
-        
+
         self.panel_1.SetSizer(sizer_1)
-        
+
         self.Layout()
 
-        self.Bind(wx.EVT_BUTTON, self.on_button_show_modal, self.button_1)
+        self.Bind(wx.EVT_BUTTON, self.show_modal, self.button_1)
         # end wxGlade
 
-    def on_button_show_modal(self, event):  # wxGlade: MyFrame.<event_handler>
+    def show_modal(self, event):  # wxGlade: MyFrame.<event_handler>
+        # event might be a button or menu event; it's not used anyway
         # instantiate dialog and set text control to initial value
         with MyDialog(self) as dlg:
             dlg.text_ctrl_1.SetValue(self.text_ctrl_1.GetValue())
@@ -58,30 +68,30 @@ class MyDialog(wx.Dialog):
         kwds["style"] = kwds.get("style", 0) | wx.DEFAULT_DIALOG_STYLE
         wx.Dialog.__init__(self, *args, **kwds)
         self.SetTitle("dialog")
-        
+
         sizer_1 = wx.BoxSizer(wx.VERTICAL)
-        
+
         static_text_1 = wx.StaticText(self, wx.ID_ANY, "Enter text:")
         sizer_1.Add(static_text_1, 0, wx.ALL, 8)
-        
+
         self.text_ctrl_1 = wx.TextCtrl(self, wx.ID_ANY, "")
         sizer_1.Add(self.text_ctrl_1, 0, wx.ALL | wx.EXPAND, 8)
-        
-        sizer_1.Add((20, 20), 1, wx.ALIGN_CENTER | wx.EXPAND, 0)
-        
+
+        sizer_1.Add((20, 20), 1, wx.EXPAND, 0)
+
         sizer_2 = wx.BoxSizer(wx.HORIZONTAL)
         sizer_1.Add(sizer_2, 0, wx.ALIGN_RIGHT | wx.ALL, 8)
-        
+
         self.button_1 = wx.Button(self, wx.ID_CANCEL, "")
         sizer_2.Add(self.button_1, 0, 0, 0)
-        
+
         self.button_2 = wx.Button(self, wx.ID_OK, "")
         self.button_2.SetDefault()
         sizer_2.Add(self.button_2, 0, 0, 0)
-        
+
         self.SetSizer(sizer_1)
         sizer_1.Fit(self)
-        
+
         self.Layout()
         # end wxGlade
 
