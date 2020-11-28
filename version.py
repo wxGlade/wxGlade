d7ffdc14a0700d68b247b68fca186de0fa775072
DietmarSchwertberger <github@schwertberger.de>
2020-11-28 23:45:09 +0100

    update version number to 1.0.0b2

--- version.py
+++ version.py
@@ -1,1 +1,1 @@
-__version__ = "1.0.0b1"  # don't forget to update also the wxglade shell script, sphinx/conf.py and wxGlade.desktop
+__version__ = "1.0.0b2"  # don't forget to update also the wxglade shell script, sphinx/conf.py and wxGlade.desktop
