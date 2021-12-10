5f986af65ee314a248edc61dca0c89e74854ede3
DietmarSchwertberger <github@schwertberger.de>
2021-12-10 20:57:56 +0100

    add test case for issue #502

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,6 +20,8 @@ class TestCodegen(WXGladeCLITest):
     def test_codegen_AllWidgets_28(self):
         self.generate('AllWidgets_28')
 
+    def test_Issue502_codegen(self):
+        self.generate('Issue502_codegen_fail', included=["perl"])
 
 if __name__ == '__main__':
     unittest.main(exit=False)
