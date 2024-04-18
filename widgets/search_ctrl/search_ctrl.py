5d73d7f36da8e8c27eedae3279c18a9da0e194b3
DietmarSchwertberger <github@schwertberger.de>
2024-04-18 19:10:06 +0200

    fix default name for SearchCtrl (issue #546)

--- widgets/search_ctrl/search_ctrl.py
+++ widgets/search_ctrl/search_ctrl.py
@@ -69,7 +69,7 @@ class EditSearchCtrl(ManagedBase, EditStylesMixin):
 
 def builder(parent, index):
     "factory function for EditSearchCtrl objects"
-    name = parent.toplevel_parent.get_next_contained_name('text_ctrl_%d')
+    name = parent.toplevel_parent.get_next_contained_name('search_ctrl_%d')
     with parent.frozen():
         editor = EditSearchCtrl(name, parent, index)
         editor.properties["style"].set_to_default()
