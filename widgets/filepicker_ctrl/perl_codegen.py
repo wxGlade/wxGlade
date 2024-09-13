26ce21dbb53bf9edcf8a7d1e657ff28ba9c683d3
Johan Vromans <jvromans@squirrel.nl>
2024-09-13 09:31:58 +0200

    (Perl) Fix properties generation.

--- widgets/filepicker_ctrl/perl_codegen.py
+++ widgets/filepicker_ctrl/perl_codegen.py
@@ -8,14 +8,16 @@ import wcodegen
 
 class PerlFilePickerCtrlGenerator(wcodegen.PerlWidgetCodeWriter):
     tmpl = '%(name)s = %(klass)s->new(%(parent)s, %(id)s, ' \
-           '"", "Select file", "Text files (*.txt)|*.txt|All files|*.*", ' \
-           'wxDefaultPosition, wxDefaultSize, %(style)s);\n'
-    prefix_style = False
+           '%(path)s, %(message)s, %(wildcard)s"' \
+           '%(style)s);\n'
     set_default_style = True
 
     def _prepare_tmpl_content(self, obj):
         wcodegen.PerlWidgetCodeWriter._prepare_tmpl_content(self, obj)
         self.has_setdefault = int(obj.properties.get('default', 0))
+        self.tmpl_dict['path'] = self.codegen.quote_str(obj.path)
+        self.tmpl_dict['wildcard'] = self.codegen.quote_str(obj.wildcard)
+        self.tmpl_dict['message'] = self.codegen.quote_str(obj.message)
         return
 
 
